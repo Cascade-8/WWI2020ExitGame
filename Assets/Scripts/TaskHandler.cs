cb6579614e1ab234be2a86376f12c9c53298c0c6
Nico Schwarz <wi20212@lehre.dhbw-stuttgart.de>
2021-04-16 02:57:04 +0200

    initial commit

---
+++ Assets/Scripts/TaskHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskHandler : MonoBehaviour
+{
+    public Collider2D task1;
+    public Collider2D task2;
+    public Collider2D task3;
+    public Collider2D task4;
+    public Collider2D task5;
+    public Collider2D task6;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
