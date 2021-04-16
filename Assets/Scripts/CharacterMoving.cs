2fdd10311d7d7cbef690a65de708d9838b119270
Nico Schwarz <wi20212@lehre.dhbw-stuttgart.de>
2021-04-16 02:24:21 +0200

    commit

--- Assets/Scripts/CharacterMoving.cs
+++ Assets/Scripts/CharacterMoving.cs
@@ -1,37 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml;
 using UnityEngine;
 
 public class CharacterMoving : MonoBehaviour
 {
     public Camera mainCamera;
-    public float walkingSpeed = 5f;
-    private Vector2 movement;
     public Rigidbody2D rb;
     public Animator characterAnimator;
     
+    public float walkingSpeed = 5f;
+    private Vector2 _movement;
+    
+    
     // Start is called before the first frame update
-    void Start()
-    {
-    }
-
+    void Start(){}
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
         
-        characterAnimator.SetFloat("Horizontal", movement.x);
-        characterAnimator.SetFloat("Vertical", movement.y);
-        characterAnimator.SetFloat("Speed", movement.sqrMagnitude);
+        characterAnimator.SetFloat("Horizontal", _movement.x);
+        characterAnimator.SetFloat("Vertical", _movement.y);
+        characterAnimator.SetFloat("Speed", _movement.sqrMagnitude);
     
     }
-
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * walkingSpeed * Time.deltaTime);
+        rb.MovePosition(rb.position + _movement * walkingSpeed * Time.deltaTime);
         mainCamera.transform.position = new Vector3(rb.position.x, rb.position.y, mainCamera.transform.position.z);
     }
 }
