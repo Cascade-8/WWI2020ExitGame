e60f61f3567594354b95354c0e1d7d232615dc4c
Nico Schwarz <wi20212@lehre.dhbw-stuttgart.de>
2021-05-06 16:11:41 +0200

    initial commit

---
+++ Assets/Scripts/Task/Mastermind/RowHandler.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+namespace Task.Mastermind
+{
+    public class RowHandler : MonoBehaviour
+    {
+
+        public GameObject[] rows = new GameObject[7];
+        public GameObject[] lamps = new GameObject[4];
+    
+    
+    }
+}
